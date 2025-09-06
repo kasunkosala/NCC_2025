b05783f1f9cbfd7faa1aff17f114cb91cd0d0c62
Aruni Liyanage <64838935+AruniRL@users.noreply.github.com>
2025-09-06 14:00:48 +0530

    Added name and index number as comment

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        //name: A.R.Liyanage
+        //IndexNo: Gs/comp/335
     }
 }
